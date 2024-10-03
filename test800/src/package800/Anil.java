92a0a8f48d73f0aacba98fe2e01a98f021a0ed4e
yalag <yalag@192.168.0.8>
2024-10-03 22:32:33 +0530

    To Commit the data

---
+++ test800/src/package800/Anil.java
@@ -0,0 +1,47 @@
+package package800;
+
+public class Anil {
+
+	public static void main(String[] args) {
+		
+		String str="jayakrishna";
+		
+		String str1="";
+		
+		
+		for(int i=10; i>=0; i--) {
+			
+			str1= str1+str.charAt(i);
+		}	
+		System.out.println(str1);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
